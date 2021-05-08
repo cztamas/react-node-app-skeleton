7047a6c7f6f09b08543349afff6939daf4432a6a
cztamas <tamas.czank@gmail.com>
2021-05-08 15:19:56 +0200

    add favicon

--- src/server/app.js
+++ src/server/app.js
@@ -2,8 +2,11 @@
 
 const path = require('path');
 const express = require('express');
+const serveFavicon = require('serve-favicon');
 
 const app = express();
+
+app.use(serveFavicon(path.join(__dirname, '../../static/favicon.ico')));
 app.set('views', path.resolve(__dirname, 'views'));
 
 app.get('/healthcheck', require('./endpoints/healthcheck/healthcheck'));
